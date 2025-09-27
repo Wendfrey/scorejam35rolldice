bfbf311196c4a6f15b867d073f99c7d16b5c7811
Wendfrey <toni.cano.redondo@gmail.com>
2025-09-27 12:51:34 +0200

    add dice on main stage, dice now uses face resource, change display from fullscren to windowed

--- Dice/dice.gd
+++ Dice/dice.gd
@@ -1,38 +1,57 @@
 extends Sprite2D
 
-var options : Array
+@export var all_possible_faces: Array[DiceFaceDataResource]
+
+var options : Array[DiceFaceDataResource]
 var interval : float = 0.1
-var timer : int = 50
+var timer : int = 10
 var previewInterval : float = 1
 var active : bool = true
 var preview : bool = false
 var isFinished : bool = false
-var rng
-var ticks
-var choice
+var rng: RandomNumberGenerator
+var ticks: int  = timer
+var choice:DiceFaceDataResource
 
+func _ready() -> void:
+	rng = RandomNumberGenerator.new()
+	generate_new_dice()
+
+## Input control
+func _unhandled_input(event: InputEvent) -> void:
+	if event.is_action_pressed("ui_accept") and ticks == timer:
+		roll()
+	if event.is_action_pressed("ui_cancel") and ticks == timer:
+		generate_new_dice()
+
+## Inicia el roll del dado
 func roll() -> void:
 	ticks = 0
 	isFinished = false
-	rng = RandomNumberGenerator.new()
 	##Iniciemos el timer con el intervalo de espera para ver el "roll" del dado
 	##Estoy abierto a mejoras
 	$TimerRoll.wait_time = interval
 	$TimerRoll.start()
 
+## Cada vez que se el timer acabe cambia la textura hasta que ticks sea igual a timer
 func _on_timer_timeout() -> void:
 	##Cuando se acabe el tiempo de espera, que haga el "roll". Si aún no ha acabado el tiempo (timer), reiniciamos el timer
 	##Sigo diciendo, estoy abierto a mejoras. Me disculpo con quien tenga que leer esto
 	ticks = ticks + 1
 	if ticks < timer:
-		var opt : int = rng.randi_range(0, options.size() - 1)
-		choice = options[opt]
-		self.texture = load(str("res://Assets/Texture/",choice))
-		$TimerRoll.start()
+		choice = options.pick_random()
+		self.texture = choice.texture
 	else:
+		$TimerRoll.stop()
 		isFinished = true
 	
-
+## Change faces of dice
+func generate_new_dice():
+	options.clear()
+	for i in range(6):
+		options.append(all_possible_faces.pick_random())
+	
+	self.texture = options.pick_random().texture
 
 ##Properties
 #func _setOptions(newValue : Array) -> void:
