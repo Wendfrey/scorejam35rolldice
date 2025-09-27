36f7e6c68d4e5419aae8268e3f3c3967156d7f6b
KroryBloody <josegregoriohndz1512@gmail.com>
2025-09-27 21:11:28 +0200

    Musikita

--- settings.gd
+++ settings.gd
@@ -1,6 +1,10 @@
 extends Panel
-@onready var button: Button = $Button
+@export var is_mainMenu:bool = false
+@onready var button: Button = $MarginContainer/VBoxContainer/Button
 
 
+func _ready() -> void:
+	if is_mainMenu:
+		button.hide()
 func _on_button_button_up() -> void:
 	get_tree().change_scene_to_file("res://MainMenu.tscn")
