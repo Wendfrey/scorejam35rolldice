c0d00defe2b7b4b1e41c2ee30705a40a39038483
KroryBloody <josegregoriohndz1512@gmail.com>
2025-09-27 13:12:14 +0200

    Tutorial en el menu principal

--- Script/main_menu.gd
+++ Script/main_menu.gd
@@ -1,6 +1,7 @@
 extends Control
 
 @export var mainGame:PackedScene
+@onready var tutocontainer: MarginContainer = $TutoContainer
 
 
 
@@ -9,7 +10,10 @@ func _on_button_play_button_up() -> void:
 
 
 func _on_button_tutorial_button_up() -> void:
-	pass # Replace with function body.
+	if tutocontainer.visible:
+		tutocontainer.hide()
+	else:
+		tutocontainer.show()
 
 
 func _on_button_setting_button_up() -> void:
