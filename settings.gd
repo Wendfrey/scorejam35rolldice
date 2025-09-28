b2b523f4b24dbeffb991504f2b91b7964db79416
Marcopole11 <nicomarcopernic@gmail.com>
2025-09-28 14:10:19 +0200

    Menu mas bonito

--- settings.gd
+++ settings.gd
@@ -1,10 +1,14 @@
 extends Panel
 @export var is_mainMenu:bool = false
 @onready var button: Button = $MarginContainer/VBoxContainer/Button
-
+@onready var background: NinePatchRect = $NinePatchRect
 
 func _ready() -> void:
 	if is_mainMenu:
 		button.hide()
+		background.self_modulate = Color("8798be")
 func _on_button_button_up() -> void:
 	get_tree().change_scene_to_file("res://MainMenu.tscn")
+	
+	
+	
